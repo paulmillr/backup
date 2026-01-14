faf966c096b9df814b2906d584cd3022564515e5
Paul Miller <paul@paulmillr.com>
2026-01-14 02:09:56 +0000

    2026.01.14

--- README.md
+++ README.md
@@ -8,6 +8,20 @@ Mirrors:
 - https://gitlab.com/paulmillr/backup
 - https://codeberg.org/paulmillr/backup
 
+## Excluded projects
+
+Test vectors & some dotfiles are excluded (>100MB git file limit), clone manually:
+
+```sh
+git clone git@github.com:paulmillr/acvp-vectors.git
+git clone git@github.com:paulmillr/eth-vectors.git
+git clone git@github.com:paulmillr/qr-code-vectors.git
+git clone git@github.com:paulmillr/noble-hashes-vectors.git
+git clone git@github.com:paulmillr/post-quantum-vectors.git
+
+git clone git@github.com:paulmillr/dotfiles-vsix.git
+```
+
 ## Usage
 
 ```sh
@@ -24,7 +38,7 @@ gpg --detach-sign --armor --sign shasum.txt
 ## --------
 
 # Verify, unpack into separate directory
-title='2025.06.20'
+title='2026.01.14'
 tar -xf $title.tar.xz
 
 # Create archive
--- list-clonable-repos.js
+++ list-clonable-repos.js
@@ -1,19 +1,29 @@
 const USER = 'paulmillr';
-const DO_NOT_CLONE = new Set(['backup', 'qr-code-vectors', 'unused-test-repo', 'paulmillr', 'paulmillr.github.io']);
+const DO_NOT_CLONE = new Set([
+  'backup', 'test-repo', 'unused-test-repo', 'paulmillr', 'paulmillr.github.io',
+
+  'acvp-vectors',
+  'eth-vectors',
+  'qr-code-vectors',
+  'noble-hashes-vectors',
+  'post-quantum-vectors',
+
+  'dotfiles-vsix',
+]);
 (async () => {
   const reqs = await Promise.all([1, 2, 3].map(function getPage(page) {
     return fetch(`https://api.github.com/users/${USER}/repos?page=${page}`).then(r => r.json());
   }));
   const repos = reqs.flat().filter(repo => !repo.archived && !repo.fork && !(DO_NOT_CLONE.has(repo.name)));
   console.log(repos.map(r => r.name))
-  const isNobleScure = r => (r.name.startsWith('noble') || r.name.startsWith('scure'))
+  const isNoble = r => (r.name.startsWith('noble'))
   const printCloneUrls = (repos) => repos.forEach(r => console.log(`git clone ${r.ssh_url}`))
-  const general = repos.filter(r => !isNobleScure(r))
-  const nobleScure = repos.filter(r => isNobleScure(r));
+  const general = repos.filter(r => !isNoble(r))
+  const nob = repos.filter(r => isNoble(r));
 
   console.log('# general')
   printCloneUrls(general)
 
-  console.log('# noble and scure')
-  printCloneUrls(nobleScure)
+  console.log('# noble')
+  printCloneUrls(nob)
 })()
